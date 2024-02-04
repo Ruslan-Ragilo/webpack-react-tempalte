3abb4aa00c75e0b5189211977ee2998761fc7f28
RagiloR <ruslanragilo7@gmail.com>
2024-02-04 12:55:03 +0300

    initial project

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import { IBuildOptions } from "./type/config";
+import type { Configuration as  DevServerConfiguration} from "webpack-dev-server";
+
+export function buildDevServer(options: IBuildOptions): DevServerConfiguration  {
+  return {
+    port: options.port,
+    open: true,
+    historyApiFallback: true
+  }
+}
